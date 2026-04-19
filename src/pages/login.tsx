0801fdff3d4070cbd57c560434cb345ba8d3e761
carlesflor94 <cf.ferrandis@gmail.com>
2026-04-19 19:17:39 +0200

    login page structure

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,28 @@
 const Login = () => {
-  return <div>Login example</div>;
+  return (
+    <div>
+      <button>
+        <img src="../assets/icons/Back-Button.svg" alt="Back button" />
+      </button>
+
+      <div>
+        <img src="../assets/images/Vector.svg" alt="Utown logo" />
+      </div>
+
+      <form>
+        <div>
+          <input type="text" placeholder="Phone number" />
+        </div>
+        <div>
+          <input type="text" placeholder="Password" />
+        </div>
+        <button type="submit">Log in</button>
+      </form>
+      <div>
+        Forgot password?<button type="button">Recover</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
