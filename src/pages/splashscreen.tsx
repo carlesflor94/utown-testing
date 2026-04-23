9249a1dd11a85d49da0f150708474c897c883479
carlesflor94 <cf.ferrandis@gmail.com>
2026-04-23 06:19:23 +0200

    added splash screen

---
+++ src/pages/splashscreen.tsx
@@ -0,0 +1,11 @@
+import logo from "../assets/images/Vector.svg";
+
+const Splash = () => {
+  return (
+    <div className="splash">
+      <img src={logo} alt="Utown app logo" />
+    </div>
+  );
+};
+
+export default Splash;
